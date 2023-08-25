8926dc206d3e4bef4de9b0c22114aa91764ad640
zohraDev <zohra.bachi@edu.itescia.fr>
2023-08-25 22:13:36 +0200

    create crud and load cv file

---
+++ db/crud.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Session
+
+from db import models, schemas
+
+
+def create_operator(db: Session, operator: schemas.Operators):
+    db_operator = models.Operators(code=operator.code, name=operator.name)
+    db.add(db_operator)
+    db.commit()
+    db.refresh(db_operator)
+    return db_operator
+
+
+def create_network_offer(db: Session, network_offer: schemas.NetWorkOffers):
+    db_network = models.NetWorkOffers(
+        code=network_offer.code,
+        long=network_offer.long,
+        lat=network_offer.lat,
+        g2_offer=network_offer.g2_offer,
+        g3_offer=network_offer.g3_offer,
+        g4_offer=network_offer.g4_offer
+    )
+    db.add(db_network)
+    db.commit()
+    db.refresh(db_network)
+    db.close()
+    return db_network
+
+
+def get_network_offer_by_coordinate(db: Session, coordinate: dict):
+    db.connection()
+    return db.query(models.NetWorkOffers).filter(
+        models.NetWorkOffers.long == coordinate["long"] and models.NetWorkOffers.lat == coordinate["lat"]
+    )
+
+
+def get_operator_by_code(db: Session, code: int):
+    return db.query(models.Operators).filter(models.Operators.code == code)
+
+
---
+++ db/load_csv_file.py
@@ -0,0 +1,61 @@
+import os
+import pathlib
+import csv
+
+from db import schemas, models
+from db import crud
+from db.database import SessionLocal, engine
+from utils.utils import lamber93_to_gps
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def fill_operator_table():
+    db = SessionLocal()
+    data = {"orange": 20801, "SFR": 20810, "Free": 2081, "Bouygue": 20820}
+
+    for name, code in data.items():
+        try:
+            operator = schemas.Operators(name=name, code=code)
+            crud.create_operator(db, operator)
+        except Exception as ex:
+            print(f"operator {name} - {code} cannot be process.\n{str(ex)}")
+            continue
+    print(f"data loading into the 'operators' is complete")
+
+
+def fill_network_offers_table():
+    db=SessionLocal()
+    try:
+        with open(f"{os.path.join(pathlib.Path(__file__).parent.resolve(), 'Sites_mobiles.csv')}", 'r') as csvfile:
+            # create the object of csv.reader()
+
+            csv_file_reader = csv.reader(csvfile, delimiter=';')
+            # skip the header
+            next(csv_file_reader, None)
+            for line, row in enumerate(csv_file_reader):
+
+                try:
+                    long, lat = lamber93_to_gps(row[1], row[2])
+                    network_offer = schemas.NetWorkOffers(
+                        code=row[0],
+                        long=format(long, '.2f'),
+                        lat=format(lat, '.2f'),
+                        g2_offer=row[3],
+                        g3_offer=row[4],
+                        g4_offer=row[5]
+
+                    )
+                    crud.create_network_offer(db, network_offer)
+                except Exception as ex:
+                    print(f"Line {line+2} cannot be process.")
+                    print(str(ex))
+                    continue
+    except FileNotFoundError:
+        print("File not found. Check the path variable and filename")
+        exit()
+    print(f"data loading into the 'networkoffers' is complete")
+
+
+fill_operator_table()
+fill_network_offers_table()
