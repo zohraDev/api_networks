fd29493378b9251bbe1f4707cece263742666389
zohraDev <73198618+zohraDev@users.noreply.github.com>
2023-10-23 17:05:09 +0200

    Update bash_file.bash

--- bash_file.bash
+++ bash_file.bash
@@ -1,2 +1,2 @@
-export PROJETC_PATH="$HOME/PycharmProjects/api_networks_test"
+export PROJETC_PATH="$HOME/PycharmProjects/api_networks"
 export PYTHONPATH="$PATH:$PROJETC_PATH:$PROJETC_PATH/db:$PROJETC_PATH/utils"
